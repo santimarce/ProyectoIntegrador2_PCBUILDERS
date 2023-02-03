45ae4253244c833e23b88045c1438ab03179b38e
santimarce <marchulo1996@gmail.com>
2023-02-03 11:21:04 -0500

    Diseño def de ingresoproducto

--- web/js/ingresoproductos.js
+++ web/js/ingresoproductos.js
@@ -1,44 +1,72 @@
 const SelectorTipoProducto = document.getElementById("ComboTipoComponente");
-var diccionarioProductos = {
+const diccionarioProductos = {
     "Laptop": "Pulgadas",
     "Desktop": "RGB",
     "Software": "Tipo",
     "Videojuegos": "Genero",
-    "MemoriaRam": ["Potencia Consumida", "Tipo", "Factor de forma"],
     "Perifericos": ["Wireless", "Wired"],
-    "TarjetaGrafica": ["Potencia Consumida", "PuertoExpansion", "GeneracionMem"],
+    "Almacenamiento": ["Potencia Consumida", "Tipo", "Factor de Forma", "Capacidad GB"],
+    "Memoria Ram": ["Potencia Consumida", "Tipo", "Factor de forma", "Capacidad GB"],
+    "Tarjeta Grafica": ["Potencia Consumida", "Puerto Expansion", "Tipo Memoria"],
     "Procesador": ["Potencia Consumida", "Socket", "Generacion", "Nucleos"],
-    "Mainboard": ["Potencia Consumida", "TipoMemoria", "Socket", "PuertoExpansion", "Tipo"],
-    "Fuente": ["Potencia Consumida", "PotenciaEntrega"]
+    "Mainboard": ["Potencia Consumida", "Tipo Memoria", "Socket", "Puertos Expansion", "Tipo"],
+    "Fuente": ["Potencia Consumida", "Potencia Entrega"]
 };
+
+const tipoCasilla = {
+    "text": ["Tipo", "Factor de Forma", "Genero", "Tipo Memoria", "Socket", "Puerto Expansion", "Factor de Forma"],
+    "number": ["Potencia Consumida", "Pulgadas", "Capacidad GB", "Puertos Expansion", "Generacion", "Nucleos", "Potencia Entrega"],
+    "radio": ["RGB", "Wireless", "Wired",]
+}
+
+var ColumnaNew = document.getElementById("ColumnaNew");
 
 SelectorTipoProducto.addEventListener('change', (event) => {
     var Seleccion = event.target.value;
-
     var valores = diccionarioProductos[Seleccion];
-    valores.forEach(valor => {
-        console.log(valor);
-    });
-
-    // for (var prop in diccionarioProductos  ) {
-    //     console.log(` ${diccionarioProductos.Perifericos[prop]}`);
-    // };
+    if (valores == "Pulgadas" || valores == "RGB" || valores == "Tipo" || valores == "Genero") {
+        ColumnaNew.innerHTML = "";
+        var tipo = generartipo(valores);
+        console.log(tipo);
+        crearnuevoinput(valores, tipo);
+    } else {
+        ColumnaNew.innerHTML = "";
+        valores.forEach(element => {
+            var tipo = generartipo(element);
+            console.log(tipo);
+            crearnuevoinput(element, tipo);
+        });
+    }
 });
 
-function crearnuevoinput(Seleccion) {
+function crearnuevoinput(Seleccion, tipo) {
     document.getElementById("ColumnaNew");
     var newRow = document.createElement("div");
     newRow.classList.add("form-group");
     var newInput = document.createElement("input");
     newInput.classList.add("form-control");
-    newInput.setAttribute("type", "text");
+    newInput.setAttribute("type", tipo);
     newInput.setAttribute("id", "generado" + Seleccion);
     newInput.setAttribute("placeholder", "Ingrese la potencia de consumo de " + Seleccion);
     var newLabel = document.createElement("label");
     newLabel.setAttribute("for", "generado" + Seleccion);
-    newLabel.innerHTML = "Ingrese " + Seleccion;
+    newLabel.innerHTML = Seleccion;
     newRow.appendChild(newLabel);
     newRow.appendChild(newInput);
-    var ColumnaNew = document.getElementById("ColumnaNew");
     ColumnaNew.appendChild(newRow);
 };
+
+// hay que generar los radio buttons y ya empezar a mandar los datos al back
+
+function generartipo(variable) {
+    var retorno = new String();
+    Object.keys(tipoCasilla).forEach(clave => {
+        tipoCasilla[clave].forEach(valorxclave => {
+            if (valorxclave == variable) {
+                retorno = clave;
+            }
+        });
+    });
+    return retorno;
+}
+
